93bc2e2ac57ae5fe189a725ee9e7407a07f169e7
Miller.Ph <minhsnow2411@gmail.com>
2020-02-26 10:56:58 -0400

    hi

---
+++ StaticClassDemo/StaticClassConsole/Program.cs
@@ -0,0 +1,42 @@
+﻿using StaticClassDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClassConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string quote = CustomPrinter.QuoteIt("640k ought to be enough memory for anyone", "Bill Gate");
+
+            Console.WriteLine(quote);
+
+            Console.WriteLine(CustomPrinter.CountVowel("What's up everybody ? Welcome back to channel. Hi ! How are ya !" +
+                "?"));
+
+            Console.WriteLine(CustomPrinter.UpperLower("What's up everybody ? Welcome back to channel. Hi ! How are ya !" +
+                "?"));
+
+            do
+            {
+                Console.WriteLine("Specify a size to a count");
+                long a = Convert.ToInt64(Console.ReadLine());
+                Console.WriteLine("Specify test type. 1 StringBuilder 2 Concetentation");
+                int testType = Convert.ToInt32(Console.ReadLine());
+
+                long result = CustomPrinter.StringBuildTest(a, testType == 1 ?
+                    CustomPrinter.StringBuilding.StringBuilder : CustomPrinter.StringBuilding.Conatenation);
+
+                Console.WriteLine("The result is " + result + " milliseconds");
+
+                Console.WriteLine("Go again ? y/n");
+
+            } while (Console.ReadLine().ToLower() != "n");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StaticClassDemo/StaticClassDemo/CustomPrinter.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClassDemo
+{
+    public static class CustomPrinter
+    {
+        public static string QuoteIt(string toQuote, string author)
+        {
+            return "\"" + toQuote + "\" - " + author;
+
+        }
+
+        public static int CountVowel(string phrase)
+        {
+            char[] vowels = { 'a', 'e', 'o', 'i', 'u' };
+            char[] charArr = phrase.ToLower().ToCharArray();
+
+            int vowelsFound = 0;
+            foreach(char v in vowels)
+            {
+                vowelsFound += charArr.Where(ch => ch == v).Count();
+            }
+
+            return vowelsFound;
+        }
+
+        public static string UpperLower(string toChange)
+        {
+            StringBuilder sb = new StringBuilder(toChange);
+
+            for(int i = 0; i<toChange.Length; i++)
+            {
+                if( i % 2 == 0)
+                {
+                    sb[i] = Char.ToUpper(sb[i]);
+                }
+                else
+                {
+                    sb[i] = Char.ToLower(sb[i]);
+                }
+            }
+            return sb.ToString();
+        }
+
+        public enum StringBuilding
+        {
+            Conatenation,
+            StringBuilder
+        }
+
+        public static long StringBuildTest(long countTo,StringBuilding testType)
+        {
+            long milliseconds = 0;
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+
+            if(testType == StringBuilding.StringBuilder)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                for(int i = 0; i<countTo; i++)
+                {
+                    sb.Append(i.ToString());
+                }
+            }
+            else
+            {
+                string test = "";
+                for(int i = 0; i< countTo; i++)
+                {
+                    test += i.ToString();
+                }
+            }
+
+            sw.Stop();
+            milliseconds = sw.ElapsedMilliseconds;
+
+            return milliseconds;
+        }
+    }
+}
